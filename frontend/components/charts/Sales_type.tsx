bc32178e802d6bf69c69e70165553af2ebc4bbb0
Madhurapabu <madhurapabu@gmail.com>
2021-09-13 21:20:17 +0530

    styled the chart

--- frontend/components/charts/Sales_type.tsx
+++ frontend/components/charts/Sales_type.tsx
@@ -1,109 +1,121 @@
-import { Flex } from "@chakra-ui/layout";
+import { Box, Flex, Heading } from "@chakra-ui/layout";
 import React, { useEffect, useState } from "react";
-import { Doughnut } from "react-chartjs-2";
+import { Doughnut, defaults } from "react-chartjs-2";
 
-const BarChart: React.FC  = ({}) => {
-
-{/* Declare Arrays and objects  */}
+const BarChart: React.FC = ({}) => {
+  {
+    /* Declare Arrays and objects  */
+  }
   const today = new Date();
   let liters = new Array();
   var Type_Total: { [k: string]: any } = {};
 
+  const day = new Date(today);
+  day.setDate(day.getDate() - 1);
+  const Sday = day.toISOString().slice(0, 10);
+
   let types = {
-      Diesel :0,
-      Petrol : 0,
-      SuperDiesel : 0,
-      SuperPetrol : 0,
-      Kerosine: 0
-  }
+    Diesel: 0,
+    Petrol: 0,
+    SuperDiesel: 0,
+    SuperPetrol: 0,
+    Kerosine: 0,
+  };
 
   type Cash = {
     fueltype: string;
     sales_liters: string;
+  };
+
+  const [chartData, setChartData] = useState({});
+
+  {
+    /* Function for Fetch Data and Populate the chart  */
   }
-  
-  const [chartData, setChartData] = useState({})
-
-
-  {/* Function for Fetch Data and Populate the chart  */}
   const fetchSales = async () => {
 
-      const day = new Date(today);
-      day.setDate(day.getDate() - 1);
-    
-      const Sday = day.toISOString().slice(0,10)
-      console.log(Sday)
-      const res = await fetch("http://localhost:5000/sales/date/"+ Sday)
-      const cash: Cash[] = await res.json()
-      cash.map((i) => {
-          
-          const hasKey = i.fueltype in Type_Total;
 
-        {/* Calculate Total saled liters from each fuel type */}
-          if (hasKey) {
-            let temp = Type_Total[i.fueltype];
-            Type_Total[i.fueltype] = (
-              Number(temp) + Number(i.sales_liters)
-            ).toFixed(2);
-          } else {
-            Type_Total[i.fueltype] = Number(i.sales_liters).toFixed(2);
-          }
+    const res = await fetch("http://localhost:5000/sales/date/" + Sday);
+    const cash: Cash[] = await res.json();
 
-        
-          if(i.fueltype == "Diesel"){
-             types.Diesel = Type_Total[i.fueltype]
-           }
-           else if (i.fueltype == "Petrol"){
-            types.Petrol = Type_Total[i.fueltype]
-           }
-           else if (i.fueltype == "Super Petrol"){
-            types.SuperPetrol = Type_Total[i.fueltype]
-           }
-           else if (i.fueltype == "Super Diesel"){
-            types.SuperDiesel = Type_Total[i.fueltype]
-           }
-           else {
-            types.Kerosine = Type_Total[i.fueltype]
-           }  
-      })
-    
-    liters.push(types.Diesel)
-    liters.push(types.Petrol)
-    liters.push(types.SuperDiesel)
-    liters.push(types.SuperPetrol)
-    liters.push(types.Kerosine)   
+    cash.map((i) => {
+      const hasKey = i.fueltype in Type_Total;
+
+      {
+        /* Calculate Total saled liters from each fuel type */
+      }
+      if (hasKey) {
+        let temp = Type_Total[i.fueltype];
+        Type_Total[i.fueltype] = (
+          Number(temp) + Number(i.sales_liters)
+        ).toFixed(2);
+      } else {
+        Type_Total[i.fueltype] = Number(i.sales_liters).toFixed(2);
+      }
+
+      if (i.fueltype == "Diesel") {
+        types.Diesel = Type_Total[i.fueltype];
+      } else if (i.fueltype == "Petrol") {
+        types.Petrol = Type_Total[i.fueltype];
+      } else if (i.fueltype == "Super Petrol") {
+        types.SuperPetrol = Type_Total[i.fueltype];
+      } else if (i.fueltype == "Super Diesel") {
+        types.SuperDiesel = Type_Total[i.fueltype];
+      } else {
+        types.Kerosine = Type_Total[i.fueltype];
+      }
+    });
+
+    liters.push(types.Diesel);
+    liters.push(types.Petrol);
+    liters.push(types.SuperDiesel);
+    liters.push(types.SuperPetrol);
+    liters.push(types.Kerosine);
 
     setChartData({
-      labels : ["Diesel", "Petrol", "Super Diesel", "Super Petrol", "Kerosine"],
+      labels: ["Diesel", "Petrol", "Super Diesel", "Super Petrol", "Kerosine"],
       datasets: [
         {
           data: liters,
-          backgroundColor:[
-            'blue',
-            'yellow',
-            'purple',
-            'orange',
-            "gray",
-          ],
-          hoverOffset: 4
+          backgroundColor: ["blue", "yellow", "purple", "orange", "gray"],
+          hoverOffset: 4,
         },
       ],
-    })
-  }
- 
+    });
+  };
+
   useEffect(() => {
     fetchSales();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-
- return(
-  <>
-    <Flex w="100%" mt="100px" ml={25} h="max-content" bg="gray.200" p={4} shadow="lg">
-      <Doughnut data={chartData}/>
-    </Flex>
-  </>
-)
+  return (
+    <>
+    
+      <Flex
+        w="100%"
+        mt="30px"
+        h="md"
+        bg="white"
+        p={2}
+        shadow="lg"
+        borderRadius="lg"
+        direction="column"
+      >
+        <Heading fontSize="lg" mb={1}> {Sday}  Day Sales </Heading>
+        <Box h="400px">
+          <Doughnut
+            data={chartData}
+            height={800}
+            width={600}
+            options={{
+              maintainAspectRatio: false,
+            }}
+          />
+        </Box>
+      </Flex>
+    </>
+  );
 };
 
 export default BarChart;
--- frontend/components/charts/cashflow.tsx
+++ frontend/components/charts/cashflow.tsx
@@ -1,43 +1,45 @@
 import { Flex } from "@chakra-ui/layout";
+import { Heading } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { Chart, Line } from "react-chartjs-2";
 
-
-const LineChart: React.FC  = ({}) => {
-
-{/* Declare Arrays and objects  */}
+const LineChart: React.FC = ({}) => {
+  {
+    /* Declare Arrays and objects  */
+  }
   const today = new Date();
   let TSales = new Array();
   let SDate = new Array();
 
   type Cash = {
-    paiedAmount: string
-  }
-  const [chartData, setChartData] = useState({})
- 
-  {/* Function for Fetch Data and Populate the chart  */}
-  const fetchSales = async () => {
-    
-    for(let i=9; i>=0; i--){
+    paiedAmount: string;
+  };
+  const [chartData, setChartData] = useState({});
 
+  {
+    /* Function for Fetch Data and Populate the chart  */
+  }
+  const fetchSales = async () => {
+    for (let i = 9; i >= 0; i--) {
       let totalIncome = 0;
 
       const day = new Date(today);
       day.setDate(day.getDate() - i);
-      SDate.push(day.getDate())
-      const Sday = day.toISOString().slice(0,10)
+      SDate.push(day.getDate());
+      const Sday = day.toISOString().slice(0, 10);
 
-      const res = await fetch("http://localhost:5000/pumperrecords/date/"+ Sday)
-      const cash: Cash[] = await res.json()
-      
+      const res = await fetch(
+        "http://localhost:5000/pumperrecords/date/" + Sday
+      );
+      const cash: Cash[] = await res.json();
+
       cash.map((i) => {
         totalIncome = totalIncome + Number(i.paiedAmount);
-        
-      })
-        TSales.push(totalIncome)   
+      });
+      TSales.push(totalIncome);
     }
     setChartData({
-      labels : SDate,
+      labels: SDate,
       datasets: [
         {
           label: "Daily Cash InFlow",
@@ -47,17 +49,31 @@ const LineChart: React.FC  = ({}) => {
           borderColor: "green",
         },
       ],
-  })
-  }
- 
+    });
+  };
+
   useEffect(() => {
     fetchSales();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
-   return(
-  <>
-    <Flex w="100%" mt="100px" ml={25} h="max-content" bg="gray.200" p={4} shadow="lg">
-      <Line data={chartData}
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <>
+      <Flex
+        w="100%"
+        mt="30px"
+        mr={5}
+        h="400px"
+        bg="White"
+        borderRadius="lg"
+        p={4}
+        shadow="lg"
+        direction="column"
+      >
+        <Heading fontSize="lg" mb={4}>
+            Daily Cash InFlow in Last 10 Days
+        </Heading>
+        <Line
+          data={chartData}
           options={{
             responsive: true,
             title: { text: "THICCNESS SCALE", display: true },
@@ -67,25 +83,26 @@ const LineChart: React.FC  = ({}) => {
                   ticks: {
                     autoSkip: true,
                     maxTicksLimit: 10,
-                    beginAtZero: true
+                    beginAtZero: true,
                   },
                   gridLines: {
-                    display: false
-                  }
-                }
+                    display: false,
+                  },
+                },
               ],
               xAxes: [
                 {
                   gridLines: {
-                    display: false
-                  }
-                }
-              ]
-            }
-          }} />
-    </Flex>
-  </>
-)
+                    display: false,
+                  },
+                },
+              ],
+            },
+          }}
+        />
+      </Flex>
+    </>
+  );
 };
 
 export default LineChart;
